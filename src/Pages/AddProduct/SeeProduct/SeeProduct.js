cc64af26fb92b295ea751a5f2583e6f953c3c5f9
sozib62 <webdevsozib62@gmail.com>
2022-11-26 06:40:04 +0600

    added delete method

--- src/Pages/AddProduct/SeeProduct/SeeProduct.js
+++ src/Pages/AddProduct/SeeProduct/SeeProduct.js
@@ -1,9 +1,73 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import toast from 'react-hot-toast';
+import Loading from '../../../Component/Loading/Loading';
 
 const SeeProduct = () => {
+
+    const { data: products = [], isLoading, refetch } = useQuery({
+        queryKey: ['products'],
+        queryFn: async () => {
+            const res = await fetch('http://localhost:5000/product');
+            const data = await res.json();
+            return data;
+        }
+    })
+
+    if (isLoading) {
+        return <Loading></Loading>
+    }
+
+    const handleDelete = id => {
+        fetch(`http://localhost:5000/product/${id}`, {
+            method: 'DELETE'
+        })
+            .then(res => res.json())
+            .then(data => {
+                // console.log(data);
+                if (data.acknowledged) {
+                    toast.success('Delete Success')
+                    refetch()
+                }
+            })
+    }
+
     return (
         <div>
+            <h1 className="text-3xl">See All Product-{products.length}</h1>
 
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Image</th>
+                            <th>Seller Name</th>
+                            <th>Product Name</th>
+                            <th>Price</th>
+                            <th>Phone</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            products.map((product, i) => <tr key={i}>
+                                <th>{i + 1}</th>
+                                <td><div className="avatar">
+                                    <div className="w-24 rounded-full">
+                                        <img src={product.image} alt='' />
+                                    </div>
+                                </div></td>
+                                <td>{product.sellerName}</td>
+                                <td>{product.productName}</td>
+                                <td>$ {product.price}</td>
+                                <td>{product.phone}</td>
+                                <td><button onClick={() => handleDelete(product._id)} className='btn btn-xs btn-error'>Delete</button></td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
--- src/Pages/Blog/Blog/Blog.js
+++ src/Pages/Blog/Blog/Blog.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Blog = () => {
     return (
-        <div>
+        <div className='mt-10'>
             <h1>this is blg</h1>
         </div>
     );
